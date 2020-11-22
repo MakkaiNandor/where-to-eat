38f5794ec55af1f35cff40c9a2f93555a7431f47
Nándor Makkai <makkainandor@gmail.com>
2020-11-22 11:55:26 +0200

    List item layout created

--- app/src/main/java/com/example/androidproject/SplashActivity.kt
+++ app/src/main/java/com/example/androidproject/SplashActivity.kt
@@ -8,7 +8,6 @@ import androidx.annotation.MainThread
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_splash)
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
