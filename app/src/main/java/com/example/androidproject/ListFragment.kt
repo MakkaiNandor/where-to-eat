9c850fac502d17c4e489e43424db6f723336da0c
Nándor Makkai <makkainandor@gmail.com>
2020-12-07 20:56:47 +0200

    Filter fragment added

--- app/src/main/java/com/example/androidproject/ListFragment.kt
+++ app/src/main/java/com/example/androidproject/ListFragment.kt
@@ -12,12 +12,6 @@ import com.example.androidproject.RetrofitInstance.api
 import com.example.androidproject.model.Restaurants
 import retrofit2.Call
 import retrofit2.Response
-import javax.security.auth.callback.Callback
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -25,32 +19,28 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val root = inflater.inflate(R.layout.fragment_list, container, false)
+
         val recyclerView = root.findViewById<RecyclerView>(R.id.rest_list)
         recyclerView.adapter = RestaurantAdapter(this.context!!, listOf())
         recyclerView.layoutManager = LinearLayoutManager(this.context)
         recyclerView.setHasFixedSize(true)
 
-        api.getRestaurants(mapOf("city" to "Chicago")).enqueue(object: retrofit2.Callback<Restaurants> {
+        val filters = if (MainActivity.searchFilters.isEmpty()) MainActivity.baseSearchFilter else MainActivity.searchFilters
+        api.getRestaurants(filters).enqueue(object: retrofit2.Callback<Restaurants> {
             override fun onResponse(call: Call<Restaurants>, response: Response<Restaurants>) {
-                Log.d("Response", "onResponse")
-                Log.d("Response", "Restaurants: ${response.body()!!.total_entries}")
-                recyclerView.adapter = RestaurantAdapter(requireContext(), response.body()!!.restaurants)
+                if(response.isSuccessful) {
+                    Log.d("Response", "onResponse")
+                    Log.d("Response", "Restaurants: ${response.body()!!.total_entries}")
+                    recyclerView.adapter = RestaurantAdapter(requireContext(), response.body()!!.restaurants)
+                }
+                else{
+                    Log.d("Response", "Failed: ${response.code()}, ${response.message()}")
+                }
             }
 
             override fun onFailure(call: Call<Restaurants>, t: Throwable) {
@@ -67,18 +57,9 @@ class ListFragment : Fragment() {
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
          *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
          * @return A new instance of fragment ListFragment.
          */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                ListFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+        fun newInstance() = ListFragment()
     }
 }
--- app/src/main/java/com/example/androidproject/MainActivity.kt
+++ app/src/main/java/com/example/androidproject/MainActivity.kt
@@ -1,19 +1,44 @@
 package com.example.androidproject
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.example.androidproject.RetrofitInstance.api
-import com.example.androidproject.model.Restaurant
-import com.example.androidproject.model.Restaurants
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val baseSearchFilter : Map<String, String> = mapOf("city" to "Chicago")
+        var searchFilters : Map<String, String> = mapOf()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val message = if(isNetworkAvailable()) "Connection OK" else "No internet connection"
+
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+
+    }
+
+    private fun isNetworkAvailable() : Boolean{
+        val manager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            val network = manager.activeNetwork ?: return false
+            val activeNetwork = manager.getNetworkCapabilities(network) ?: return false
+            return when {
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
+                else -> false
+            }
+        }
+        else {
+            return manager.activeNetworkInfo?.isConnected ?: false
+        }
     }
 }
